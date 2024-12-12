0959c89c656bed0b38209de8b53d5c5d54e11c78
Moisés <jimenezmartinmoises@gmail.com>
2024-12-12 07:53:58 +0100

    Add first routes

--- src/routes/order.lazy.jsx
+++ src/routes/order.lazy.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState, useContext } from "react";
-import Pizza from "./Pizza";
-import Cart from "./Cart";
-import { CartContext } from "./contexts";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import Pizza from "../Pizza.jsx";
+import Cart from "../Cart.jsx";
+import { CartContext } from "../contexts.jsx";
+
+export const Route = createLazyFileRoute("/order")({
+  component: Order,
+});
 
 const intl = new Intl.NumberFormat("es-ES", {
   style: "currency",
   currency: "EUR",
 });
 
-export default function Order() {
+function Order() {
   const [pizzaTypes, setPizzaTypes] = useState([]);
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
