e4735faebc93806bed05948571ec5be3387348b8
Moisés <jimenezmartinmoises@gmail.com>
2024-12-15 08:43:08 +0100

    Uncontrolled Form

---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,41 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useMutation } from "@tanstack/react-query";
+import postContact from "../api/postContact.js";
+
+export const Route = createLazyFileRoute("/contact")({
+  component: ContactRoute,
+});
+
+function ContactRoute() {
+  const mutation = useMutation({
+    mutationFn: function (e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      return postContact(
+        formData.get("name"),
+        formData.get("email"),
+        formData.get("message"),
+      );
+    },
+  });
+
+  if (mutation.isError) {
+    return <h1>LOL No</h1>;
+  }
+
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      {mutation.isSuccess ? (
+        <h3>Submitted!</h3>
+      ) : (
+        <form onSubmit={mutation.mutate}>
+          <input name="name" placeholder="name" />
+          <input name="email" placeholder="Email" type="email" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+}
