ccecb335b0a71574c24ad8f5d6bf6a3971376375
moi-j <jimenezmartinmoises@gmail.com>
2024-12-21 07:07:10 +0100

    Use waitFor function

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,5 @@
-import {renderHook } from "@testing-library/react";
 import { expect, test, vi } from "vitest";
+import {renderHook, waitFor } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
 
@@ -19,4 +19,13 @@ test("gives null when first called", async () => {
   fetch.mockResponseOnce(JSON.stringify(testPizza));
   const {result} = renderHook(() => usePizzaOfTheDay());
   expect(result.current).toBeNull();
+});
+
+test("to call the API and five back the pizza of the day", async () => {
+  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  const {result} = renderHook(( () => usePizzaOfTheDay()));
+  await waitFor(() => {
+    expect(result.current).toEqual(testPizza);
+  });
+  expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
 });
